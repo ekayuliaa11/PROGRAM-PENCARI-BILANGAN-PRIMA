38c5582d487ce81bfeb58ae937a3145e4ff5ef59
ekayuliaa11 <45647196+ekayuliaa11@users.noreply.github.com>
2019-01-04 00:06:30 +0700

    Add files via upload

---
+++ tugas 2. MENAMPILKAN BIL PRIMA 1-100.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+int main ()
+{
+    int i,j,k;
+
+    for (i=1;i<=100;i++){
+        k=0;
+        for(j=1;j<=i;j++){
+            if(i%j==0){
+                k+=1;
+            }
+        }
+        if(k==2){
+            cout<<i<<"";
+        }
+    }
+        return 0;
+}
